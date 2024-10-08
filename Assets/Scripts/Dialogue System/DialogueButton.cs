16db3ee134d006604b07412208f66e99c9f90a3e
Keagon King <keagonk@gmail.com>
2024-10-08 14:59:51 -0500

    Dialogue System Fully Working With Input System
    You can talk to npc, you can do it with keyboard or mouse.

--- Assets/Scripts/Dialogue System/DialogueButton.cs
+++ Assets/Scripts/Dialogue System/DialogueButton.cs
@@ -5,32 +5,59 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace DialogueSystem
 {
-    public class DialogueButton : MonoBehaviour, IPointerClickHandler
+    public class DialogueButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
         public Dialogue dialogue;
         public TMP_Text text;
+        public Image image;
 
         public Color selectedColor;
+        public Color hoveredColor;
+        public Color originalColor;
 
         private void Awake()
         {
+            image = GetComponent<Image>();
             dialogue.hasBeenRead = false;
+            selectedColor = Color.gray;
+            originalColor = image.color;
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            PressButton();
+            Debug.Log("OnPointerClick");
+        }
+
+        public void PressButton()
+        {
             DialogueManager.instance.selectDialogueLine(dialogue);
             DialogueManager.instance.lastClickedDialogueButton = this;
             updateReadStatus();
+            Debug.Log("PressButton");
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            image.color = hoveredColor;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            image.color = originalColor;
         }
 
         private void updateReadStatus()
         {
-            dialogue.hasBeenRead = true;
-            text.color = selectedColor;
+            if (!dialogue.hasBeenRead)
+            {
+                dialogue.hasBeenRead = true;
+                text.color = selectedColor;
+            }
         }
     }
 }
