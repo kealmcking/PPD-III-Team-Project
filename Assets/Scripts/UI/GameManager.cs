4679ed4b604c1b546229b8ca4f1c65b9972a4b82
ajjacobsen101 <132110217+ajjacobsen101@users.noreply.github.com>
2024-10-18 12:01:13 -0600

    Updated UI, Added functionality to Tracker

--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -1,6 +1,9 @@
 using Input;
+using NUnit.Framework;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -11,8 +14,6 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
    // private audioManager audioManager;
 
-    
-    
 
     [SerializeField] GameObject player;
     // [SerializeField] SkinnedMeshRenderer playerSkinnedMeshRenderer;
@@ -38,6 +39,22 @@ public class GameManager : MonoBehaviour
     public GameObject MenuActive => menuActive;
     private bool isPauseActive;
     public bool IsPauseActive => isPauseActive;
+
+    [Header("Bindings")]
+    private List<KillerClueData> killerChoices = new List<KillerClueData>();
+    private List<MotiveClueData> motiveChoices = new List<MotiveClueData>();
+    private List<WeaponClueData> weaponChoices = new List<WeaponClueData>();
+    private List<RoomClueData> roomChoices = new List<RoomClueData>();
+    [Header("Lists")]
+    public List<Toggle> killerToggles = new List<Toggle>();
+    public List<Toggle> motiveToggles = new List<Toggle>();
+    public List<Toggle> weaponToggles = new List<Toggle>();
+    public List<Toggle> roomToggles = new List<Toggle>();
+
+    private int suspectListIndex;
+    private int weaponListIndex;
+    private int roomListIndex;
+    private int motiveListIndex;
 
     public bool InventoryActive { get; private set; }
     public bool CraftTableActive { get; private set; }
@@ -90,6 +107,91 @@ public class GameManager : MonoBehaviour
 
         characterIndex = PlayerPrefs.GetInt("SelectedCharacter");
         playerController.UpdatePlayerCharacter(characterIndex);
+    }
+    private void UpdateToggleUI(Toggle toggle, BaseClueData data)
+    {
+        //TextMeshProUGUI text = toggle.GetComponentInChildren<TextMeshProUGUI>();
+        //if (text != null)
+        //    text.text = data.Name;
+        toggle.image.sprite = data.Icon;
+        //toggle.isOn = true;
+    }
+
+    public void ClueFound(Toggle toggle)
+    {
+        toggle.isOn = true;
+    }
+
+    public void OnEnable()
+    {
+        EventSheet.SendAllClues += HandleClues;
+        EventSheet.SendKillerClue += HandleKillerClue;
+        EventSheet.SendClueToTracker += HandleNewClue;
+    }
+    public void OnDisable()
+    {
+        EventSheet.SendAllClues -= HandleClues;
+        EventSheet.SendKillerClue -= HandleKillerClue;
+        EventSheet.SendClueToTracker -= HandleNewClue;
+    }
+
+    public void HandleNewClue(Clue clue)
+    {
+        switch (clue.Data)
+        {
+            case KillerClueData suspect:
+                
+                ClueFound(killerToggles.ElementAt(killerChoices.IndexOf(suspect)));
+                Destroy(clue.gameObject);
+                break;
+            case MotiveClueData motive:
+
+                ClueFound(motiveToggles.ElementAt(motiveChoices.IndexOf(motive)));
+                Destroy(clue.gameObject);
+                break;
+            case WeaponClueData weapon:
+
+                ClueFound(weaponToggles.ElementAt(weaponChoices.IndexOf(weapon)));
+                Destroy(clue.gameObject);
+                break;
+            case RoomClueData room:
+
+                ClueFound(roomToggles.ElementAt(roomChoices.IndexOf(room)));
+                Destroy(clue.gameObject);
+                break;
+        }
+    }
+    public void HandleClues(List<BaseClueData> clues)
+    {
+        foreach (BaseClueData clue in clues)
+        {
+            switch (clue)
+            {
+                case KillerClueData suspect:
+                    killerChoices.Add(suspect);
+                    UpdateToggleUI(killerToggles.ElementAt(killerChoices.IndexOf(suspect)), suspect);
+
+                    break;
+                case MotiveClueData motive:
+                    motiveChoices.Add(motive);
+                    UpdateToggleUI(motiveToggles.ElementAt(motiveChoices.IndexOf(motive)), motive);
+                    break;
+                case WeaponClueData weapon:
+                    weaponChoices.Add(weapon);
+                    UpdateToggleUI(weaponToggles.ElementAt(weaponChoices.IndexOf(weapon)), weapon);
+                    break;
+                case RoomClueData room:
+                    roomChoices.Add(room);
+                    UpdateToggleUI(roomToggles.ElementAt(roomChoices.IndexOf(room)), room);
+                    break;
+            }
+        }
+    }
+    public void HandleKillerClue(KillerClueData clue)
+    {
+        
+        killerChoices.Add(clue);
+        UpdateToggleUI(killerToggles.ElementAt(killerChoices.IndexOf(clue)), clue);
     }
 
     public void DisplayCharacterUI()
@@ -312,5 +414,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-   
+    
+
+
 }
