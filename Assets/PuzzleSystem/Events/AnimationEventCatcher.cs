a6b72fda7e76c7125701ef97a081e75b78dd02e7
Drake Strait <Dstrait3773@gmail.com>
2024-10-21 02:14:06 -0700

    Working foot steps (not dynamic)
    +Foot step SFX
    -Removed code from playerController
    
    (Needs adjustment)

--- Assets/PuzzleSystem/Events/AnimationEventCatcher.cs
+++ Assets/PuzzleSystem/Events/AnimationEventCatcher.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class AnimationEventCatcher : MonoBehaviour
 {
+    private audioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio Manager").GetComponent<audioManager>();
+    }
+
     public void ThrowEvent()
     {
         EventSheet.ThrowAnimationEvent?.Invoke();
@@ -12,4 +19,24 @@ public class AnimationEventCatcher : MonoBehaviour
     {
         EventSheet.ItemColliderAnimationEvent?.Invoke();
     }
+
+    private void footStep()
+    {
+        audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepWalkVol);
+
+        //if (!_isCrouching && !_isSprinting)          //Walking
+        //{
+        //    audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepWalkVol);
+        //}
+        //else if (!_isCrouching && _isSprinting)   //Sprinting
+        //{
+        //    audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepRunVol);
+        //}
+        //else if (!_isSprinting && _isCrouching)   //Crouching
+        //{
+        //    audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepCrouchVol);
+        //}
+    }
 }
+
+
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -213,22 +213,6 @@ public class playerController : MonoBehaviour
         ApplyGravity();
     }
 
-    private void footStep()
-    {
-        if (!_isCrouching && !_isSprinting)          //Walking
-        {
-            audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepWalkVol);
-        }
-        else if (!_isCrouching && _isSprinting)   //Sprinting
-        {
-            audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepRunVol);
-        }
-        else if (!_isSprinting && _isCrouching)   //Crouching
-        {
-            audioManager.PlaySFX(audioManager.footStepWood[UnityEngine.Random.Range(0, audioManager.footStepWood.Length)], audioManager.footStepCrouchVol);
-        }
-    }
-
     private void ApplyGravity()
     {
         if (charController.isGrounded)
